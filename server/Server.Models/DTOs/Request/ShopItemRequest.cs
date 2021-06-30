9b9b425f5da2208f4a4c1a98692959e3e1eab0d8
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-06-30 22:54:16 +0300

    Create DTO models

---
+++ server/Server.Models/DTOs/Request/ShopItemRequest.cs
@@ -0,0 +1,7 @@
+namespace Server.Models.DTOs.Request
+{
+    public class ShopItemRequest
+    {
+        
+    }
+}
---
+++ server/Server.Models/DTOs/Response/ShopListResponse.cs
@@ -0,0 +1,7 @@
+namespace Server.Models.DTOs.Response
+{
+    public class ShopListResponse
+    {
+        
+    }
+}
