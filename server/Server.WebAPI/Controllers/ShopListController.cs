6cb6a6c516bb0d3bb808bb2b5dfa3cf9fed69d4b
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-07-02 18:18:14 +0300

    Create controllers

---
+++ server/Server.WebAPI/Controllers/ShopListController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShopListController : ControllerBase
+    {
+
+    }
+}
