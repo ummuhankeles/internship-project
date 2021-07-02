2b33455434880b50b4af38cee241eca69c6e7a2b
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-07-02 21:45:02 +0300

    Add base apireturn

---
+++ server/Server.Models/ApiResponse.cs
@@ -0,0 +1,24 @@
+using Server.Models.Enums;
+
+namespace Server.Models
+{
+    public class ApiResponse
+    {
+        public ApiResponse(ApiResponseType _type)
+        {
+            Type = _type;
+        }
+        public ApiResponse(object _data)
+        {
+            Data = _data;
+        }
+        public ApiResponse(object _data, ApiResponseType _type)
+        {
+            Data = _data;
+            Type = _type;
+        }
+
+        public object Data { get; set; } = null;
+        public ApiResponseType Type { get; set; } = ApiResponseType.Ok;
+    }
+}
