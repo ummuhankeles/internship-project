be1892b00a5bb9edf569a62dab94f06c38b6c04e
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-07-02 19:20:53 +0300

    Add and implement creating shop list

--- server/Server.Service/Concrete/ShopListService.cs
+++ server/Server.Service/Concrete/ShopListService.cs
@@ -1,18 +1,26 @@
 using System;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
 using Server.Data;
 using Server.Models.DTOs.Request;
 using Server.Models.DTOs.Response;
+using Server.Models.Entities;
 using Server.Service.Abstract;
 
 namespace Server.Service.Concrete
 {
     public class ShopListService : IShopListService
     {
+        private readonly IMapper mapper;
         private readonly XContext context;
-        public ShopListService(XContext _context)
+        private readonly ILogger<ShopListService> logger;
+
+        public ShopListService(XContext _context, IMapper _mapper, ILogger<ShopListService> _logger)
         {
             context = _context;
+            mapper = _mapper;
+            logger = _logger;
         }
 
         public Task<ShopListResponse> GetById(Guid id)
@@ -25,9 +33,25 @@ namespace Server.Service.Concrete
             throw new NotImplementedException();
         }
 
-        public Task<ShopListResponse> InsertAsync(ShopListRequest value)
+        public async Task<ShopListResponse> InsertAsync(ShopListRequest value)
         {
-            throw new NotImplementedException();
+        re:
+            var newShopList = mapper.Map<ShopList>(value);
+            await context.AddAsync(newShopList);
+
+            try
+            {
+                if (await context.SaveChangesAsync() > 0)
+                {
+                    return mapper.Map<ShopListResponse>(newShopList);
+                }
+                return null;
+            }
+            catch (System.Exception ex)
+            {
+                logger.LogWarning(ex.InnerException.Message);
+                goto re;
+            }
         }
 
         public Task<bool> UpdateAsync(ShopListRequest value)
