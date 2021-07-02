20f4e1c472bb512ab97833a0765fe671dcbfd5e3
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-07-02 19:20:53 +0300

    Add and implement creating shop list

--- server/Server.WebAPI/Controllers/ShopListController.cs
+++ server/Server.WebAPI/Controllers/ShopListController.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Server.Models.DTOs.Request;
+using Server.Service.Abstract;
 
 namespace Server.WebAPI.Controllers
 {
@@ -6,6 +9,20 @@ namespace Server.WebAPI.Controllers
     [Route("[controller]")]
     public class ShopListController : ControllerBase
     {
+        private readonly IShopListService shopListService;
+
+        public ShopListController(IShopListService _shopListService)
+        {
+            shopListService = _shopListService;
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> CreateList(ShopListRequest model)
+        {
+            var result = await shopListService.InsertAsync(model);
+            //todo: get result as a response class
+            return result != null ? Ok(result) : BadRequest("");
+        }
 
     }
 }
